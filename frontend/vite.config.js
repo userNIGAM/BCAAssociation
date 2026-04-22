f861ce6d59712d5b6478e20a18b4c9db9e384b40
Nigam Subedi <nigam333nigam@gmail.com>
2026-04-22 21:46:52 +0545

    changed the port address

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ import tailwindcss from "@tailwindcss/vite"
 export default defineConfig({
   plugins: [react(), tailwindcss()],
    server: {
-    port: 3000,
+    port: 5173,
     proxy: {
       '/api': 'http://localhost:5000'
     }
