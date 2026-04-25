b2f8b8ec7532c2bde33b07920762134ead6970e2
Nigam Subedi <nigam333nigam@gmail.com>
2026-04-25 22:14:34 +0545

    added env

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,10 @@ dotenv.config();
 connectDb();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: process.env.CLIENT_URL,
+  credentials: true
+}));
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
