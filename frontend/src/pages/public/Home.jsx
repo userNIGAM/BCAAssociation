7da35a5683c97636feccf381396b4c7addc53569
Nigam Subedi <nigam333nigam@gmail.com>
2026-05-08 20:48:57 +0545

    added news section on the home section

--- frontend/src/pages/public/Home.jsx
+++ frontend/src/pages/public/Home.jsx
@@ -1,11 +1,12 @@
 import Hero from "../../components/Hero";
 import Event from "../../components/Event";
 import ContactSection from "../../components/ContactSection";
-
+import News from "./News"
 export default function Home() {
   return (
     <>
       <Hero />
+      <News />
       <Event />
       <ContactSection />
     </>
